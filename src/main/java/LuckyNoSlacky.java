4278fe56191ca3992b7a766d1d2363b07adacbe3
lol-hi <luckheng.koh@gmail.com>
2026-08-17 01:03:28 +0800

    Implement greeting and exit flow

--- src/main/java/LuckyNoSlacky.java
+++ src/main/java/LuckyNoSlacky.java
@@ -2,6 +2,8 @@
  * Starts the LuckyNoSlacky chatbot.
  */
 public class LuckyNoSlacky {
+    private static final String DIVIDER = "\n____________________________________________________________\n";
+
     public static void main(String[] args) {
         String banner = "     .--\"\"\"\"\"--.\n"
                 + "   /  /^\\   /^\\  \\\n"
@@ -10,7 +12,11 @@ public class LuckyNoSlacky {
                 + "   \\ '---------' /\n"
                 + "     '-._____.-'\n"
                 + "    [NO SLACKING]\n"
-                + " LuckyNoSlacky is here to help!\n";
-        System.out.println(banner);
+                + " LuckyNoSlacky is here to help!";
+        String hello = "  Hello, I'm LuckyNoSlacky!\n  What can I do for you?";
+        System.out.print(DIVIDER + banner + DIVIDER + hello + DIVIDER);
+
+        String goodbye = "  Bye, hope to see you again soon!";
+        System.out.print(DIVIDER + goodbye + DIVIDER);
     }
 }
