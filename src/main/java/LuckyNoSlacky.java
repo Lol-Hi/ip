ade41d157a6658e1e1c5dea33b399a375390ab82
lol-hi <luckheng.koh@gmail.com>
2026-08-17 01:38:29 +0800

    Add command echoing
    The chatbot needs to accept user input before it can support commands.
    Read input from standard input, echo non-exit commands, and exit on bye.

--- src/main/java/LuckyNoSlacky.java
+++ src/main/java/LuckyNoSlacky.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 /**
  * Starts the LuckyNoSlacky chatbot.
  */
+
 public class LuckyNoSlacky {
-    private static final String DIVIDER = "\n____________________________________________________________\n";
+    private static final String DIVIDER = "\n  ____________________________________________________________\n";
+
+    private Scanner userScanner;
+
+    LuckyNoSlacky() {
+        userScanner = new Scanner(System.in);
+    }
+    private static void printReply(String output) {
+        System.out.print(DIVIDER + "  " + output + DIVIDER);
+    }
+
+    private static void echo(String input) {
+        printReply(input);
+    }
 
     private void greet() {
         String banner = "     .--\"\"\"\"\"--.\n"
@@ -12,19 +28,32 @@ public class LuckyNoSlacky {
                 + "   \\ '---------' /\n"
                 + "     '-._____.-'\n"
                 + "    [NO SLACKING]\n"
-                + " LuckyNoSlacky is here to help!";
-        String hello = "  Hello, I'm LuckyNoSlacky!\n  What can I do for you?";
-        System.out.print(DIVIDER + banner + DIVIDER + hello + DIVIDER);
+                + "  LuckyNoSlacky is here to help!";
+        String hello = "Hello, I'm LuckyNoSlacky!\n  What can I do for you?";
+        System.out.print(DIVIDER + banner);
+        printReply(hello);
     }
 
     private void exit() {
-        String goodbye = "  Bye, hope to see you again soon!";
-        System.out.print(DIVIDER + goodbye + DIVIDER);
+        String goodbye = "Bye, hope to see you again soon!";
+        printReply(goodbye);
+    }
+
+    private void chatLoop() {
+        while (userScanner.hasNextLine()) {
+            String userInput = userScanner.nextLine();
+            if (userInput.equalsIgnoreCase("bye")) {
+                return;
+            }
+            echo(userInput);
+        }
+        // TODO: possible improvement: for the chat loop to return error codes for exit handle
     }
 
     public static void main(String[] args) {
         LuckyNoSlacky lucky = new LuckyNoSlacky();
         lucky.greet();
+        lucky.chatLoop();
         lucky.exit();
     }
 }
